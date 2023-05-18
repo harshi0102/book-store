663d6614b74716511a527bf591bd7109d09b6b38
Harshika Govind <harshika0102@gmail.com>
2023-05-19 01:07:47 +0530

    created new branch and added code for styling of book

--- src/components/BookElement.js
+++ src/components/BookElement.js
@@ -1,39 +1,101 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { deleteBook } from '../redux/books/booksSlice';
+// eslint-disable jsx-props-no-spreading
+/* eslint-disable react/prop-types */
+import React, { useEffect } from 'react';
+import { checkPropTypes } from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { CircularProgressBar } from '@tomik23/react-circular-progress-bar';
+import { getBooks, deleteBook } from '../redux/books/booksSlice';
 
-function BookElement({ title, author, id }) {
+const props = {
+  id: 0,
+  percent: Math.floor(Math.random() * 100 + 1), // is required
+  colorSlice: '#00a1ff',
+  colorCircle: '#00a1ff',
+  fontColor: '#365b74',
+  fontSize: '1.6rem',
+  fontWeight: 400,
+  size: 100,
+  opacity: 10,
+  stroke: 10,
+  strokeBottom: 5,
+  speed: 60,
+  cut: 0,
+  rotation: -90,
+  unit: '%',
+  textPosition: '0.35em',
+  animationOff: false,
+  inverse: false,
+  round: false,
+  number: false,
+  linearGradient: ['#00a1ff', '#00a1ff'],
+};
+
+function BookElement() {
+  const bookShow = useSelector((state) => state.books);
   const dispatch = useDispatch();
 
-  const handleDelete = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
+  const clearHandler = (id) => {
     dispatch(deleteBook(id));
   };
-
+  props.checkPropTypes = {
+    percent: checkPropTypes.random,
+  };
   return (
-    <li>
-      <div className="bookinfo">
-        <h2 className="book-title">{title}</h2>
-        <p className="book-author">{author}</p>
-        {id && (
-          <a href="/" id={id} className="deletebtn" onClick={handleDelete}>
-            Remove
-          </a>
-        )}
+    <li className="bookList">
+      <div className="bookContainer">
+        <div>
+          {bookShow.map((book) => (
+            <div key={book.id} className="card">
+              <div>
+                <h3 className="category">{book.category}</h3>
+                <h1 className="bookTitle">{book.title}</h1>
+                <h2 className="bookAuthor">{book.author}</h2>
+                <div className="element">
+                  <span className="bookElement">Comments</span>
+                  <span className="rightBorder" />
+                  <span>
+                    <button
+                      type="button"
+                      className="clear"
+                      onClick={() => {
+                        clearHandler(book.id);
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </span>
+                  <span className="rightBorder" />
+                  <span className="bookElement edit">Edit</span>
+                </div>
+              </div>
+              <div>
+                <span className="progress">
+                  {/* eslint-disable-next-line */}
+                  <CircularProgressBar {...props} />
+                  <span className="progReport">
+                    <p className="propPercent">{`${props.percent}%`}</p>
+                    <p className="rate">completed</p>
+                  </span>
+                </span>
+              </div>
+              <span className="distribution" />
+              <div className="chapter">
+                <h2>CURRENT CHAPTER</h2>
+                <h3>CHAPTER 17</h3>
+                <button type="button" className="progButton">
+                  UPDATE PROGRESS
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </li>
   );
 }
-
-BookElement.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  id: PropTypes.string,
-};
-
-BookElement.defaultProps = {
-  id: null,
-};
 
 export default BookElement;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,71 +1,49 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const INSERT_BOOK = 'bookstore/books/INSERT_BOOK';
-const DELETE_BOOK = 'bookstore/books/DELETE_BOOK';
-const GET_BOOKS = 'bookstore/books/GET_BOOKS';
+const INSERT_BOOK = './bookstore/bookreducer/INSERT_BOOK';
+const DELETE_BOOK = './bookstore/bookreducer/DELETE_BOOK';
+const BOOK_FAILURE = './bookstore/bookreducer/BOOK_FAILURE';
 
-export const insertBook = createAsyncThunk(INSERT_BOOK, async (book, { dispatch }) => {
-  await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/7J88ab57HvQlIbDye75B/books/', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
-    body: JSON.stringify(book),
-  });
-  dispatch({
-    type: INSERT_BOOK,
-    payload: book,
-  });
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps';
+const apiKey = '7J88ab57HvQlIbDye75B';
+
+const initialState = [];
+export const insertBook = (books) => ({
+  type: INSERT_BOOK,
+  payload: books,
 });
 
-export const getBooks = createAsyncThunk(GET_BOOKS, async (args, { dispatch }) => {
-  const response = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/7J88ab57HvQlIbDye75B/books/', {
-    method: 'GET',
-  });
-  const data = await response.json();
-  const bookIDs = Object.entries(data);
-  const books = [];
-  bookIDs.forEach(([key, book]) => {
-    const id = key;
-    const { title, author, category } = book[0];
-    books.push({
-      id, title, author, category,
-    });
-  });
-  dispatch({
-    type: GET_BOOKS,
-    payload: books,
-  });
-  return books;
-});
+export const getBooks = () => async (dispatch) => {
+  await axios.get(`${baseUrl}/${apiKey}/books`).then(
+    (response) => dispatch(insertBook(response.data)),
+    (err) => dispatch({ type: BOOK_FAILURE, err }),
+  );
+};
 
-export const deleteBook = createAsyncThunk(DELETE_BOOK, async (id) => {
-  await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/7J88ab57HvQlIbDye75B/books/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  return id;
-});
+export const listBook = (book) => async (dispatch) => {
+  await axios
+    .post(`${baseUrl}/${apiKey}/books`, book)
+    .then(() => dispatch(getBooks()));
+};
 
-const booksSlice = createSlice({
-  name: 'books',
-  initialState: [],
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(insertBook.fulfilled, (state, action) => {
-        state.push(action.payload);
-      })
-      .addCase(deleteBook.fulfilled, (state, action) => {
-        const id = action.payload;
-        return state.filter((book) => book.id !== id);
-      })
-      .addCase(getBooks.fulfilled, (state, action) => action.payload);
-  },
-});
+export const deleteBook = (id) => async (dispatch) => {
+  await axios
+    .delete(`${baseUrl}/${apiKey}/books/${id}`)
+    .then(() => dispatch(getBooks()));
+};
 
-export default booksSlice.reducer;
+export default function bookReducer(state = initialState, action) {
+  switch (action.type) {
+    case INSERT_BOOK: {
+      const books = Object.entries(action.payload);
+      return books.map((book) => ({
+        id: book[0],
+        ...book[1][0],
+      }));
+    }
+    case DELETE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
+}
